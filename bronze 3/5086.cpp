331b38e798daad8ddb75d006048209702e11b88d
hsssj4 <hongseongjin123@gmail.com>
2025-09-19 00:13:54 +0900

    add 5086

---
+++ bronze 3/5086.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 1, b = 1;
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+		if(a%b==0){
+			cout << "multiple" << endl;
+		}
+		else if(b%a==0){
+			cout << "factor" << endl;
+		}
+		else{
+			cout << "neither" << endl;
+		}
+	}
+}
